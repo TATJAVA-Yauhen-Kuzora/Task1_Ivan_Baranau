0e4f1edb2efa5f441671e870a1006d0eb2533c84
TATJAVA-Yauhen-Kuzora <kuzora-home@tut.by>
2017-06-23 10:53:55 +0300

    Add unit tests for subtask1

---
+++ test/main/subtask1/DataProviderSubTask1.java
@@ -0,0 +1,27 @@
+package main.subtask1;
+
+import org.testng.annotations.DataProvider;
+
+public class DataProviderSubTask1 {
+
+	@DataProvider
+	public Object[][] dp1() { // positive tests for method isTwoFirstTwoLast
+		return new Object[][] { new Object[] { true, "0000" }, //
+				new Object[] { true, "9999" }, //
+				new Object[] { true, "5418" }, //
+				new Object[] { false, "1234" }, //
+				new Object[] { false, "7466" }, //
+		};
+	}
+
+	@DataProvider
+	public Object[][] dp2() {// negative tests for method isTwoFirstTwoLast
+		return new Object[][] { new Object[] { "----" }, //
+				new Object[] { "\nce" }, //
+				new Object[] { "___!" }, //
+				new Object[] { "-5442" }, //
+				new Object[] { "74655" }, //
+				new Object[] { "+=+?" }, //
+		};
+	}
+}
---
+++ test/main/subtask1/NumberExaminerTest.java
@@ -0,0 +1,30 @@
+package main.subtask1;
+
+import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class NumberExaminerTest {
+	private NumberExaminer exam;
+
+	@BeforeMethod
+	public void beforeMethod() {
+		exam = new NumberExaminer();
+	}
+
+	@Test(groups = { "methods" }, dataProvider = "dp1", dataProviderClass = DataProviderSubTask1.class)
+	public void tst_method_isTwoFirstTwoLast(boolean expected, String number) {
+		Assert.assertEquals(exam.isTwoFirstTwoLast(number), expected);
+	}
+
+	@Test(groups = { "exceptions" }, dataProvider = "dp2", dataProviderClass = DataProviderSubTask1.class, expectedExceptions = IllegalArgumentException.class)
+	public void tst_exception_isTwoFirstTwoLast(String number) {
+		exam.isTwoFirstTwoLast(number);
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+		exam = null;
+	}
+}
